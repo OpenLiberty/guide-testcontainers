e13d649dadf95207d31522c578399930cd9a3622
Gilbert Kwan <gkwan@ca.ibm.com>
2024-02-05 15:54:52 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -6,7 +6,7 @@ cd ../postgres
 
 docker build -t postgres-sample .
 docker run --name postgres-container -p 5432:5432 -d postgres-sample
-sleep 20
+sleep 10
 docker logs postgres-container
 
 cd ../finish
@@ -29,6 +29,6 @@ docker stop postgres-container
 docker rm postgres-container
 
 # TEST 2:  Running the test by using Testcontainers
-mvn -ntp -q clean package
+mvn -ntp -q clean
 mvn -ntp verify
 
