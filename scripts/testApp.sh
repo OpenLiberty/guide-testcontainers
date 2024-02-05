f28989ba682c15eaf5c0554946d90be67ea9dd1b
Gilbert Kwan <gkwan@ca.ibm.com>
2024-02-05 17:36:42 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -euxo pipefail
 
-# TEST 1: Running the test by local runtime
+# TEST 1:  Running the test by using Testcontainers
+mvn -ntp -q clean package
+
+docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
+
+mvn -ntp verify
+
+# TEST 2: Running the test by local runtime
 cd ../postgres
 
 docker build -t postgres-sample .
+
 docker run --name postgres-container -p 5432:5432 -d postgres-sample
-sleep 10
-docker logs postgres-container
 
 cd ../finish
 
@@ -24,7 +30,3 @@ mvn -ntp failsafe:verify
 
 docker stop postgres-container
 docker rm postgres-container
-
-# TEST 2:  Running the test by using Testcontainers
-mvn -ntp clean package verify
-
