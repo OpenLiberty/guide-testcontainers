12c116bbf7bcab608fddc94aa7239d3f3080615a
Gilbert Kwan <gkwan@ca.ibm.com>
2024-02-05 15:35:11 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-# TEST 1:  Running the test by using Testcontainers
-mvn -ntp -q clean package
-
-docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
-
-mvn -ntp verify
-
-# TEST 2: Running the test by local runtime
+# TEST 1: Running the test by local runtime
 cd ../postgres
 
 docker build -t postgres-sample .
@@ -20,7 +13,7 @@ cd ../finish
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    liberty:create liberty:install-feature liberty:deploy
+    -q clean liberty:create liberty:install-feature liberty:deploy
 mvn -ntp liberty:start
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
@@ -30,3 +23,8 @@ mvn -ntp failsafe:verify
 
 docker stop postgres-container
 docker rm postgres-container
+
+# TEST 2:  Running the test by using Testcontainers
+mvn -ntp -q clean package
+mvn -ntp verify
+
