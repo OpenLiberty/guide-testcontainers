ceddd33de13fb8382f2b38efb44878ef513cd6d4
Karthik Raja B <53909984+KarthikRaja1724@users.noreply.github.com>
2025-02-26 22:01:02 +0530

    code refactoring

--- finish/src/test/java/it/io/openliberty/guides/inventory/SystemResourceIT.java
+++ finish/src/test/java/it/io/openliberty/guides/inventory/SystemResourceIT.java
@@ -45,8 +45,8 @@ public class SystemResourceIT {
     // end::getLogger1[]
 
     private static final String DB_HOST = "postgres";
-    private static final String POSTGRES_PASSWORD = "adminpwd";
     private static final int DB_PORT = 5432;
+    private static final String POSTGRES_PASSWORD = "adminpwd";
     // tag::postgresImage[]
     private static ImageFromDockerfile postgresImage
         = new ImageFromDockerfile("postgres-sample")
