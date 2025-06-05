674224b53571a40db158b3827132e3923ba47d2f
George Zhang <geetransit@gmail.com>
2025-06-05 17:35:22 -0400

    Update to use maven wrapper (#184)
    * Update to use mvnw
    
    * Fix bugs on cloud hosted guide
    
    * Simplify fix for cloud hosted guide
    
    * Update README.adoc
    
    * Bump org.postgresql:postgresql from 42.7.5 to 42.7.6 in /start
    
    Bumps [org.postgresql:postgresql](https://github.com/pgjdbc/pgjdbc) from 42.7.5 to 42.7.6.
    - [Release notes](https://github.com/pgjdbc/pgjdbc/releases)
    - [Changelog](https://github.com/pgjdbc/pgjdbc/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/pgjdbc/pgjdbc/compare/REL42.7.5...REL42.7.6)
    
    ---
    updated-dependencies:
    - dependency-name: org.postgresql:postgresql
      dependency-version: 42.7.6
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /start
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.testcontainers:testcontainers from 1.21.0 to 1.21.1 in /finish
    
    Bumps [org.testcontainers:testcontainers](https://github.com/testcontainers/testcontainers-java) from 1.21.0 to 1.21.1.
    - [Release notes](https://github.com/testcontainers/testcontainers-java/releases)
    - [Changelog](https://github.com/testcontainers/testcontainers-java/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/testcontainers/testcontainers-java/compare/1.21.0...1.21.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.testcontainers:testcontainers
      dependency-version: 1.21.1
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /finish
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.postgresql:postgresql from 42.7.5 to 42.7.6 in /finish
    
    Bumps [org.postgresql:postgresql](https://github.com/pgjdbc/pgjdbc) from 42.7.5 to 42.7.6.
    - [Release notes](https://github.com/pgjdbc/pgjdbc/releases)
    - [Changelog](https://github.com/pgjdbc/pgjdbc/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/pgjdbc/pgjdbc/compare/REL42.7.5...REL42.7.6)
    
    ---
    updated-dependencies:
    - dependency-name: org.postgresql:postgresql
      dependency-version: 42.7.6
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
 # TEST 1:  Running the test by using Testcontainers
 docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
 export TESTCONTAINERS_RYUK_DISABLED=true
-mvn -ntp clean verify
+./mvnw -ntp clean verify
 
 # TEST 2: Running the test by local runtime
 cd ../postgres
@@ -12,16 +13,16 @@ docker build -t postgres-sample .
 docker run --name postgres-container -e POSTGRES_PASSWORD=adminpwd -p 5432:5432 -d postgres-sample
 
 cd ../finish
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean compile test-compile liberty:create liberty:install-feature liberty:deploy
-mvn -ntp liberty:start
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp liberty:start
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -ntp failsafe:verify
 
 docker stop postgres-container
 docker rm postgres-container
